94583524fe431f8661849c17e75f5376d57dad9f
yu0ki <i0t0o0y0u0k0i@gmail.com>
2021-03-10 05:29:41 +0000

    [Finish]Railsを学ぼう-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ae68b80f8c770454fbbc70cccd85ad5dbd34f25d8461d19c9f2af5c012c0367f12bae04b992410c9708e5990238ea1507e0296c7be9c38b61b60ad1ff11db0ee'
